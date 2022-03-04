6bc559d04f984882bc8b23b5d53a455ae01179d9
John Dietrich <john@thoughtlesslabs.com>
2022-03-03 20:57:30 -0500

    Basic functionality working

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ const items = document.querySelectorAll("div.all-items")
 
 // Take input from text box and store in database
 function addItem() {
-    if ( this.parentElement.querySelector('.task-item').value !== "" && this.parentElement.querySelector('.task-quant').value !== "") {
-        const newDiv = this.parentElement.cloneNode(true);
+    if ( this.parentElement.parentElement.parentElement.parentElement.querySelector('.task-item').value !== "" && this.parentElement.parentElement.parentElement.parentElement.querySelector('.task-quant').value !== "") {
+        const newDiv = this.parentElement.parentElement.parentElement.parentElement.cloneNode(true);
         const delButton = newDiv.querySelector('.item-button')
         delButton.addEventListener("click", deleteItem);
         delButton.classList.remove("btn-success");
@@ -20,25 +20,25 @@ function addItem() {
 
         // Add edit button
         const editButton = document.createElement('button')
-        editButton.classList.add('col-auto', 'mr-2', 'btn', 'btn-warning', 'item-button');
+        editButton.classList.add('btn', 'btn-warning', 'item-button');
         editButton.innerText = "Edit";
         editButton.type = "button";
         editButton.addEventListener("click", editItem);
-        newDiv.prepend(editButton);
+        newDiv.querySelector('.action-buttons').prepend(editButton);
 
         // Add new div to bottom of list
-        this.parentElement.parentElement.append(newDiv);
+        this.parentElement.parentElement.parentElement.parentElement.parentElement.append(newDiv);
 
         // Reset entry field
-        this.nextElementSibling.value = "";
-        this.nextElementSibling.nextElementSibling.value = "";
+        this.parentElement.parentElement.parentElement.nextElementSibling.value = "";
+        this.parentElement.parentElement.parentElement.nextElementSibling.nextElementSibling.value = "";
     }
 }
 
 // Remove item from list
 function deleteItem() {
     // loop through all items so we can delete all?
-    this.parentElement.remove();
+    this.parentElement.parentElement.parentElement.parentElement.remove();
 }
 
 function editItem() {
@@ -46,8 +46,8 @@ function editItem() {
     this.classList.add('btn-outline-success');
     this.innerText = "Save";
     this.addEventListener("click", saveItem);
-    this.parentElement.querySelector('.task-quant').disabled = false;
-    this.parentElement.querySelector('.task-item').disabled = false;
+    this.parentElement.parentElement.parentElement.parentElement.querySelector('.task-quant').disabled = false;
+    this.parentElement.parentElement.parentElement.parentElement.querySelector('.task-item').disabled = false;
 }
 
 function saveItem() {
@@ -56,8 +56,8 @@ function saveItem() {
     this.innerText = "Edit";
     this.removeEventListener('click', saveItem)
     this.addEventListener("click", editItem);
-    this.parentElement.querySelector('.task-quant').disabled = true;
-    this.parentElement.querySelector('.task-item').disabled = true;
+    this.parentElement.parentElement.parentElement.parentElement.querySelector('.task-quant').disabled = true;
+    this.parentElement.parentElement.parentElement.parentElement.querySelector('.task-item').disabled = true;
 }
 
 // Draw a line through item indicating completion
